97ef4b6025ae0a1f893b5c13d7a6e1f8dd3009cd
Spencer Brown <spencerb21@live.com>
2023-12-11 11:51:21 +1000

    Generate directly as individual strings

--- gen_punctuation.py
+++ gen_punctuation.py
@@ -4,32 +4,26 @@ This requires iterating over the whole Unicode character set, which doesn't chan
 It's more efficient to do this earlier.
 """
 import sys
-
+import pprint
 import unicodedata
 
 
-# punctuation: _ASCII and Unicode punctuation characters_ as defined at
-# <https://spec.commonmark.org/0.30/#ascii-punctuation-character> and
-# <https://spec.commonmark.org/0.30/#unicode-punctuation-character>
-COLUMNS = 48
-
-
-def build() -> list[str]:
+def build() -> set[str]:
     """Build the characters list."""
-    characters = [
+    characters = {
         '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',',
         '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\',
         ']', '^', '_', '`', '{', '|', '}', '~'
-    ]
+    }
     for i in range(sys.maxunicode + 1):
         c = chr(i)
         if unicodedata.category(c).startswith("P"):
-            characters.append(c)
+            characters.add(c)
     return characters
 
 
 def write_module() -> None:
-    chars = ''.join(build())
+    chars = build()
 
     prefix = []
     postfix = []
@@ -48,13 +42,15 @@ def write_module() -> None:
             raise ValueError('No end comment!')
         postfix.extend(f.readlines())
 
+    formatted = pprint.pformat(chars, width=72, compact=True).splitlines()
+
     with open('mistletoe/core_tokens.py', 'w', encoding='utf8') as f:
         f.writelines(prefix)
-        f.write('# See gen_punctuation.py.\n')
-        f.write('punctuation = set(\n')
-        for i in range(0, len(chars), COLUMNS):
-            f.write(f'    {chars[i:i+COLUMNS]!r}\n')
-        f.write(')\n')
+        f.write('# Precomuted, iterating all Unicode is quite slow. See gen_punctuation.py.\n')
+        # First line here, all others indented by 8.
+        f.write(f'punctuation = {formatted[0]}\n')
+        for line in formatted[1:]:
+            f.write(f'        {line}\n')
         f.writelines(postfix)
 
 
